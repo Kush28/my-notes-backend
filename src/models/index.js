eeff539940701c26f1c02701cd534af0691116f7
Kushal Mukherjee <kushal.mukherjee28exm@gmail.com>
2020-07-20 05:27:29 +0530

    DB connection setup

---
+++ src/models/index.js
@@ -0,0 +1,21 @@
+import mongoose from 'mongoose'
+
+import User from './user'
+
+const connectDb = () => {
+  return mongoose.connect(
+    'mongodb+srv://kushal:w9Zp1hXqskIQ3B84@cluster0-p1sez.mongodb.net/bongo_rongo?retryWrites=true&w=majority',
+    {
+      useNewUrlParser: true,
+      useCreateIndex: true,
+      useFindAndModify: false,
+      useUnifiedTopology: true,
+    },
+  )
+}
+
+const models = { User }
+
+export { connectDb }
+
+export default models
