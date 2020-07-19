eb90e9a1368ae409658de7dc27ee014f7ad98dcd
Kushal Mukherjee <kushal.mukherjee28exm@gmail.com>
2020-07-19 21:20:12 +0530

    ping and posg, openapi spec

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,13 @@
 var express = require('express')
 var router = express.Router()
 
-/* GET home page. */
-router.get('/', function (req, res, next) {
-  res.render('index', { title: 'Express' })
+router.get('/ping', function (req, res, next) {
+  res.render('ping', { title: 'Express' })
+})
+
+router.post('/pong', function(req, res, next){
+  console.log(req.body)
+  res.render('pong', { title: 'Express' })
 })
 
 module.exports = router
